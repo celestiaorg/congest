d4a10cffc352b9138f14f1ec4ff98b799e7e56b2
sweexordious <chamirachid1@gmail.com>
2024-11-13 13:51:23 +0400

    chore: enable bbr

--- scripts/init_install.sh
+++ scripts/init_install.sh
@@ -26,44 +26,44 @@ fi
 
 # Load the BBR module
 echo "Loading BBR module..."
-#modprobe tcp_bbr
+modprobe tcp_bbr
 
 # Verify if the BBR module is loaded
-#if lsmod | grep -q "tcp_bbr"; then
-#  echo "BBR module loaded successfully."
-#else
-#  echo "Failed to load BBR module."
-#  exit 1
-#fi
+if lsmod | grep -q "tcp_bbr"; then
+  echo "BBR module loaded successfully."
+else
+  echo "Failed to load BBR module."
+  exit 1
+fi
 
 # Add BBR to the list of available congestion control algorithms
-#echo "Updating sysctl settings..."
-#sysctl -w net.core.default_qdisc=fq
-#sysctl -w net.ipv4.tcp_congestion_control=bbr
+echo "Updating sysctl settings..."
+sysctl -w net.core.default_qdisc=fq
+sysctl -w net.ipv4.tcp_congestion_control=bbr
 
 # Enable MPTCP
-#sysctl -w net.mptcp.enabled=1
+sysctl -w net.mptcp.enabled=1
 
 # Set the path manager to ndiffports
-#sysctl -w net.mptcp.mptcp_path_manager=ndiffports
+sysctl -w net.mptcp.mptcp_path_manager=ndiffports
 
 # Specify the number of subflows
-#SUBFLOWS=16
-#sysctl -w net.mptcp.mptcp_ndiffports=$SUBFLOWS
+SUBFLOWS=16
+sysctl -w net.mptcp.mptcp_ndiffports=$SUBFLOWS
 
 # Make the changes persistent across reboots
-#echo "Making changes persistent..."
-#echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-#echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "Making changes persistent..."
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 
 #Verify the current TCP congestion control algorithm
-#current_algo=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
-#if [ "$current_algo" == "bbr" ]; then
-#  echo "Successfully switched to BBR congestion control algorithm."
-#else
-#  echo "Failed to switch to BBR. Current algorithm is $current_algo."
-#  exit 1
-#fi
+current_algo=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
+if [ "$current_algo" == "bbr" ]; then
+  echo "Successfully switched to BBR congestion control algorithm."
+else
+  echo "Failed to switch to BBR. Current algorithm is $current_algo."
+  exit 1
+fi
 
 echo "Script completed successfully."
 
@@ -85,27 +85,18 @@ git checkout $CELESTIA_APP_COMMIT
 make install
 make txsim-install
 
-export
-
 celestia-appd config chain-id $CHAIN_ID
-
-echo 1
 
 celestia-appd init --chain-id=$CHAIN_ID --home $CELES_HOME $MONIKER
 
-echo 2
-
 cd $HOME
 
-echo 3
 # Get the hostname
 hostname=$(hostname)
 
-echo 4
 # Parse the first part of the hostname
 parsed_hostname=$(echo $hostname | awk -F'-' '{print $1 "-" $2}')
 
-echo 5
 mv payload/$parsed_hostname/node_key.json $HOME/$CELES_HOME/config/node_key.json
 
 mv payload/$parsed_hostname/priv_validator_key.json $HOME/$CELES_HOME/config/priv_validator_key.json
@@ -125,7 +116,6 @@ cp -r payload/$parsed_hostname/keyring-test $HOME/$CELES_HOME
 # run txsim script which starts a sleep timer and txsim in a different tmux session
 source payload/txsim.sh
 
-echo 6
 
 # Get the hostname of the machine
 HOSTNAME=$(hostname)
@@ -145,6 +135,5 @@ else
     COMMAND+=" --log_level=\"error\""
 fi
 
-echo 7
 # Execute the command and redirect output to the log file
 eval $COMMAND 2>&1 | tee -a "$LOG_FILE"
