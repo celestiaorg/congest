ed936ba76105c28867f7c0000e24e01257380b5f
sweexordious <chamirachid1@gmail.com>
2024-11-19 16:27:48 +0100

    chore: tm native + BBR

--- scripts/init_install.sh
+++ scripts/init_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CELESTIA_APP_COMMIT="7e75c8098da7f59364c581ddeeef5030eba79720"
+CELESTIA_APP_COMMIT="f0e752c1fa395fec3c629ac824c3a98a08acfadf"
 CELES_HOME=".celestia-app"
 MONIKER="validator"
 ARCHIVE_NAME="payload.tar.gz"
@@ -26,46 +26,45 @@ fi
 
 # Load the BBR module
 echo "Loading BBR module..."
-#modprobe tcp_bbr
+modprobe tcp_bbr
 
 # Verify if the BBR module is loaded
-#if lsmod | grep -q "tcp_bbr"; then
-#  echo "BBR module loaded successfully."
-#else
-#  echo "Failed to load BBR module."
-#  exit 1
-#fi
+if lsmod | grep -q "tcp_bbr"; then
+  echo "BBR module loaded successfully."
+else
+  echo "Failed to load BBR module."
+  exit 1
+fi
 
 # Add BBR to the list of available congestion control algorithms
-#echo "Updating sysctl settings..."
-#sysctl -w net.core.default_qdisc=fq
-#sysctl -w net.ipv4.tcp_congestion_control=bbr
+echo "Updating sysctl settings..."
+sysctl -w net.core.default_qdisc=fq
+sysctl -w net.ipv4.tcp_congestion_control=bbr
 
 # Enable MPTCP
-#sysctl -w net.mptcp.enabled=1
-
+sysctl -w net.mptcp.enabled=1
 # Set the path manager to ndiffports
-#sysctl -w net.mptcp.mptcp_path_manager=ndiffports
+sysctl -w net.mptcp.mptcp_path_manager=ndiffports
 
 # Specify the number of subflows
-#SUBFLOWS=16
-#sysctl -w net.mptcp.mptcp_ndiffports=$SUBFLOWS
+SUBFLOWS=16
+sysctl -w net.mptcp.mptcp_ndiffports=$SUBFLOWS
 
 # Make the changes persistent across reboots
-#echo "Making changes persistent..."
-#echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-#echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "Making changes persistent..."
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 
 #Verify the current TCP congestion control algorithm
-#current_algo=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
-#if [ "$current_algo" == "bbr" ]; then
-#  echo "Successfully switched to BBR congestion control algorithm."
-#else
-#  echo "Failed to switch to BBR. Current algorithm is $current_algo."
-#  exit 1
-#fi
+current_algo=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
+if [ "$current_algo" == "bbr" ]; then
+  echo "Successfully switched to BBR congestion control algorithm."
+else
+  echo "Failed to switch to BBR. Current algorithm is $current_algo."
+  exit 1
+fi
 
-#echo "Script completed successfully."
+echo "Script completed successfully."
 
 tar -xzf /root/$ARCHIVE_NAME -C /root/
 
@@ -121,7 +120,7 @@ cp -r payload/$parsed_hostname/keyring-test $HOME/$CELES_HOME
 HOSTNAME=$(hostname)
 
 # Base command
-COMMAND="celestia-appd start --force-no-bbr"
+COMMAND="celestia-appd start"
 
 # Define log file path
 LOG_FILE="/root/logs"
