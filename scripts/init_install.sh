f40773dd0b33ff3711b273af933fee811ef0d732
sweexordious <chamirachid1@gmail.com>
2024-11-23 09:39:07 +0100

    chore: add quic p2p traces

--- scripts/init_install.sh
+++ scripts/init_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CELESTIA_APP_COMMIT="f0e752c1fa395fec3c629ac824c3a98a08acfadf"
+CELESTIA_APP_COMMIT="908dbfdb635186a0b7cc263b3811ea1622bd087f"
 CELES_HOME=".celestia-app"
 MONIKER="validator"
 ARCHIVE_NAME="payload.tar.gz"
