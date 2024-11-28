2e555e36d783f154e9a6b7a655fc165c089feca3
sweexordious <chamirachid1@gmail.com>
2024-11-28 21:08:55 +0100

    chore: stop network

--- stop_network.sh
+++ stop_network.sh
@@ -2,24 +2,39 @@
 
 # Set default SSH key location
 DEFAULT_SSH_KEY="~/.ssh/do"
+
 # Allow user to override the SSH key location
 SSH_KEY=${SSH_KEY:-$DEFAULT_SSH_KEY}
 
+
+# Rest of your script goes here
+TIMEOUT=120
+
 # Fetch the IP addresses from Pulumi stack outputs
-pulumi stack output -j > ./payload/ips.json
 STACK_OUTPUT=$(pulumi stack output -j)
-echo $STACK_OUTPUT
 DROPLET_IPS=$(echo "$STACK_OUTPUT" | jq -r '.[]')
 
-mkdir -p traces
-
-stop_network() {
+# Function to boot a node
+stop_node() {
   local IP=$1
-  echo "stopping network $IP -----------------------"
-  ssh -i "$SSH_KEY" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@"$IP" "killall celestia-appd &"
+  {
+    echo "stopping celestia-appd $IP -----------------------------------------------------"
+    ssh -i "$SSH_KEY" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$IP" \
+    "tmux new -d -s stop 'source /root/payload/stop.sh'"
+    echo "stop complete for $IP"
+  } &
+
+  PID=$!
+  (sleep $TIMEOUT && kill -HUP $PID) 2>/dev/null &
+
+  if wait $PID 2>/dev/null; then
+    echo "$IP: stopping finished within timeout"
+  else
+    echo "$IP: stopping operation timed out"
+  fi
 }
 
-# Loop through the IPs and run the download logs function
+# Loop through the IPs and run the boot command in parallel
 for IP in $DROPLET_IPS; do
-  stop_network "$IP"
+  stop_node "$IP"
 done
