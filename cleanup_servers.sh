ccf0f1b66b45c15592ed21e7ec71457436eb9002
sweexordious <chamirachid1@gmail.com>
2024-11-28 09:44:34 +0100

    chore: basic version of server cleanup

---
+++ cleanup_servers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set default SSH key location
+DEFAULT_SSH_KEY="~/.ssh/do"
+# Allow user to override the SSH key location
+SSH_KEY=${SSH_KEY:-$DEFAULT_SSH_KEY}
+
+# Fetch the IP addresses from Pulumi stack outputs
+pulumi stack output -j > ./payload/ips.json
+STACK_OUTPUT=$(pulumi stack output -j)
+echo $STACK_OUTPUT
+DROPLET_IPS=$(echo "$STACK_OUTPUT" | jq -r '.[]')
+
+# Function to transfer and uncompress files on the remote server
+remove_data() {
+  local IP=$1
+  echo "cleaning up $IP -----------------------"
+  ssh -i "$SSH_KEY" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@$IP" "bash payload/cleanup_server.sh"
+}
+
+# Loop through the IPs and run the transfer and uncompress in parallel
+for IP in $DROPLET_IPS; do
+  remove_data "$IP"
+done
---
+++ scripts/cleanup_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+killall celestia-appd
+rm -rf /root/payload
+rm -rf /root/payload.tar.gz
+rm -rf /root/.celestia-app
+rm -rf /root/celestia-app
+rm -rf /root/logs
+rm -rf /root/go/bin/*
