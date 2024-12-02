ba1a02589325f9f0dda723a4d97aa5de3a239127
sweexordious <chamirachid1@gmail.com>
2024-12-02 12:10:43 +0100

    chore: quic debug peeer error

--- scripts/init_install.sh
+++ scripts/init_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CELESTIA_APP_COMMIT="afa3d19dbe19fb03849d6bc9da05f7853a3ca7e9"
+CELESTIA_APP_COMMIT="fa7c0a6f40bec49c670117af08e643c48ea57807"
 CELES_HOME=".celestia-app"
 MONIKER="validator"
 ARCHIVE_NAME="payload.tar.gz"
