33b01fb06766199a97a57dfa86b3d550605de363
sweexordious <chamirachid1@gmail.com>
2024-12-02 10:40:45 +0100

    chore: quic remove tls

--- scripts/init_install.sh
+++ scripts/init_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CELESTIA_APP_COMMIT="f1afa268b9192c1d8eec20e91d1b5223b1a9c65c"
+CELESTIA_APP_COMMIT="764d16aeda1107d741e8fbd53350a9b78b52423a"
 CELES_HOME=".celestia-app"
 MONIKER="validator"
 ARCHIVE_NAME="payload.tar.gz"
