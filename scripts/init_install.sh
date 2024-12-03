23be79926d61c02fdd97ed7c370530be3641ed2f
sweexordious <chamirachid1@gmail.com>
2024-12-03 19:08:45 +0100

    chore: disable bbr

--- scripts/init_install.sh
+++ scripts/init_install.sh
@@ -19,65 +19,65 @@ systemctl start chrony
 #export GOOGLE_APPLICATION_CREDENTIALS="/root/payload/congest-remote-key-gbq.json"
 
 # Ensure the script is run as root
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root. Please run with sudo or as root."
-  exit 1
-fi
+#if [ "$(id -u)" -ne 0 ]; then
+#  echo "This script must be run as root. Please run with sudo or as root."
+#  exit 1
+#fi
+#
+## Load the BBR module
+#echo "Loading BBR module..."
+#modprobe tcp_bbr
+#
+## Verify if the BBR module is loaded
+#if lsmod | grep -q "tcp_bbr"; then
+#  echo "BBR module loaded successfully."
+#else
+#  echo "Failed to load BBR module."
+#  exit 1
+#fi
+#
+## Add BBR to the list of available congestion control algorithms
+#echo "Updating sysctl settings..."
+#sysctl -w net.core.default_qdisc=fq
+#sysctl -w net.ipv4.tcp_congestion_control=bbr
+#
+## Enable MPTCP
+#sysctl -w net.mptcp.enabled=1
+## Set the path manager to ndiffports
+#sysctl -w net.mptcp.mptcp_path_manager=ndiffports
+#
+## Specify the number of subflows
+#SUBFLOWS=16
+#sysctl -w net.mptcp.mptcp_ndiffports=$SUBFLOWS
+#
+## Make the changes persistent across reboots
+#echo "Making changes persistent..."
+#echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+#echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+#
+##Verify the current TCP congestion control algorithm
+#current_algo=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
+#if [ "$current_algo" == "bbr" ]; then
+#  echo "Successfully switched to BBR congestion control algorithm."
+#else
+#  echo "Failed to switch to BBR. Current algorithm is $current_algo."
+#  exit 1
+#fi
 
-# Load the BBR module
-echo "Loading BBR module..."
-modprobe tcp_bbr
-
-# Verify if the BBR module is loaded
-if lsmod | grep -q "tcp_bbr"; then
-  echo "BBR module loaded successfully."
-else
-  echo "Failed to load BBR module."
-  exit 1
-fi
-
-# Add BBR to the list of available congestion control algorithms
-echo "Updating sysctl settings..."
-sysctl -w net.core.default_qdisc=fq
-sysctl -w net.ipv4.tcp_congestion_control=bbr
-
-# Enable MPTCP
-sysctl -w net.mptcp.enabled=1
-# Set the path manager to ndiffports
-sysctl -w net.mptcp.mptcp_path_manager=ndiffports
-
-# Specify the number of subflows
-SUBFLOWS=16
-sysctl -w net.mptcp.mptcp_ndiffports=$SUBFLOWS
-
-# Make the changes persistent across reboots
-echo "Making changes persistent..."
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-
-#Verify the current TCP congestion control algorithm
-current_algo=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
-if [ "$current_algo" == "bbr" ]; then
-  echo "Successfully switched to BBR congestion control algorithm."
-else
-  echo "Failed to switch to BBR. Current algorithm is $current_algo."
-  exit 1
-fi
-
-echo "Script completed successfully."
+#echo "Script completed successfully."
 
 tar -xzf /root/$ARCHIVE_NAME -C /root/
 
 #source ./vars.sh
 
 # increase udp buffers
-sudo sysctl -w net.core.rmem_max=16777216
-sudo sysctl -w net.core.wmem_max=16777216
-sudo sysctl -w net.core.rmem_default=8388608
-sudo sysctl -w net.core.wmem_default=8388608
-sudo sysctl -w net.ipv4.udp_mem="8388608 8388608 16777216"
-sudo sysctl -w net.ipv4.udp_rmem_min=1638400
-sudo sysctl -w net.ipv4.udp_wmem_min=1638400
+#sudo sysctl -w net.core.rmem_max=16777216
+#sudo sysctl -w net.core.wmem_max=16777216
+#sudo sysctl -w net.core.rmem_default=8388608
+#sudo sysctl -w net.core.wmem_default=8388608
+#sudo sysctl -w net.ipv4.udp_mem="8388608 8388608 16777216"
+#sudo sysctl -w net.ipv4.udp_rmem_min=1638400
+#sudo sysctl -w net.ipv4.udp_wmem_min=1638400
 
 sudo snap install go --channel=1.22/stable --classic
 
