5cbc6a37d03fdbe59e969c7fd6fe45da8d3ab7dc
sweexordious <chamirachid1@gmail.com>
2024-12-04 18:27:20 +0100

    chore: use 500 bytes tm native + bbr

--- scripts/init_install.sh
+++ scripts/init_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CELESTIA_APP_COMMIT="c4bd29ddf8d6aa63c1064b71dd3569001bb99eb9"
+CELESTIA_APP_COMMIT="183604d3804a8a0ab1af367a506ab4ff2754202c"
 CELES_HOME=".celestia-app"
 MONIKER="validator"
 ARCHIVE_NAME="payload.tar.gz"
@@ -25,39 +25,39 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Load the BBR module
-#echo "Loading BBR module..."
-#modprobe tcp_bbr
+echo "Loading BBR module..."
+modprobe tcp_bbr
 
 # Verify if the BBR module is loaded
-#if lsmod | grep -q "tcp_bbr"; then
-#  echo "BBR module loaded successfully."
-#else
-#  echo "Failed to load BBR module."
-#  exit 1
-#fi
+if lsmod | grep -q "tcp_bbr"; then
+  echo "BBR module loaded successfully."
+else
+  echo "Failed to load BBR module."
+  exit 1
+fi
 
 # Add BBR to the list of available congestion control algorithms
 echo "Updating sysctl settings..."
 sysctl -w net.core.default_qdisc=fq
-sysctl -w net.ipv4.tcp_congestion_control=reno
+sysctl -w net.ipv4.tcp_congestion_control=bbr
 
 # Enable MPTCP
-#sysctl -w net.mptcp.enabled=1
-## Set the path manager to ndiffports
-#sysctl -w net.mptcp.mptcp_path_manager=ndiffports
+sysctl -w net.mptcp.enabled=1
+# Set the path manager to ndiffports
+sysctl -w net.mptcp.mptcp_path_manager=ndiffports
 
 # Specify the number of subflows
-#SUBFLOWS=16
-#sysctl -w net.mptcp.mptcp_ndiffports=$SUBFLOWS
+SUBFLOWS=16
+sysctl -w net.mptcp.mptcp_ndiffports=$SUBFLOWS
 
 # Make the changes persistent across reboots
-#echo "Making changes persistent..."
-#echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-#echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "Making changes persistent..."
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 
 #Verify the current TCP congestion control algorithm
 current_algo=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
-if [ "$current_algo" == "reno" ]; then
+if [ "$current_algo" == "bbr" ]; then
   echo "Successfully switched to BBR congestion control algorithm."
 else
   echo "Failed to switch to BBR. Current algorithm is $current_algo."
